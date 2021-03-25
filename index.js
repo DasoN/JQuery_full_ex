d15c58b05b52c37bd62448639770c8988673ce2f
DasoN <the.liom.rus@gmail.com>
2021-03-26 00:16:36 +0300

    update nav

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-$('.nav-link').hover(function(){
+$('.nav-item-inner').hide()
+
+$('.nav > li').hover(function(){
   $(this).addClass('active')
+  let cThis = this
+  $(this).on('click', function(){
+    $(cThis).children().last().show()
+  })
 }, function(){
+  $('.nav-item-inner').hide()
   $(this).removeClass('active')
 })
