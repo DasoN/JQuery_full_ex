b0bfc472915abc185faa7b5065e14379cbef0760
DasoN <the.liom.rus@gmail.com>
2021-03-27 16:07:20 +0300

    upload new jquery functional

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-"# stunning-guacamole" 
+# Application
+Here I am using JQuery to show useful animation in. This site is only example of Jquery.
+
+Installed: jquery-3.6.0, jquery-ui-1.12.1, bootstrap 5, slick, avgrund
+
+
+## Installation
+
+Just drag an Html in your browser (I am using Chrome)
+
+```bash
+index.html
+```
+
+## Usage
+
+You can click in SideBar or SideBar components and see animation. Just click on everything to see it:) 
--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 $('.nav-item-inner').hide()
+$('nav > a > img').hide()
+
+//using slick
+$('.slider').slick({
+      infinite: true,
+      dots: true,
+      focusOnSelect: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      speed: 400,
+      autoplay: true,
+      autoplaySpeed: 2000
+});
+//using avgrund
+$('#showModal').avgrund({
+  width: 380, // max is 640px
+  height: 280, // max is 350px
+  showClose: true, // switch to 'true' for enabling close button
+  showCloseText: 'close', // type your text for close button
+  closeByEscape: true, // enables closing popup by 'Esc'..
+  closeByDocument: true, // ..and by clicking document itself
+  enableStackAnimation: false, // another animation type
+  openOnEvent: true, // set to 'false' to init on load
+  setEvent: 'click', // use your event like 'mouseover', 'touchmove', etc.
+  template: 'Comming soon...' // or function (elem) { }, or $('.content')
+});
 
 $('.nav > li').hover(function(){
   $(this).addClass('active')
@@ -9,4 +35,17 @@ $('.nav > li').hover(function(){
 }, function(){
   $('.nav-item-inner').hide()
   $(this).removeClass('active')
+})
+
+$('.aside-show').on('click', function(){
+  $('main > aside').css({
+    "opacity": "0"
+  })
+  $('nav > a > img').show()
+})
+
+$('nav > a > img').on('click', function(){
+  $('main > aside').css({
+    "opacity": "100%"
+  })
 })
